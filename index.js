9ee65a7a3e4b7d8f3db8171ed0576ede68a439e7
Andrej <andrejtodoroski@yahoo.com>
2024-02-23 20:19:59 -0500

    loops using while and for

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let thankNames = []
+    for (let i = 0; i < names.length; i++){
+        thankNames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankNames
+}
+
+function countDown(num){
+    while(num >= 0 ){
+        console.log(num)
+        num--
+    }
+}
